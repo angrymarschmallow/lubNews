ff680b37fd970755c371df6d9a95ce16f2e3c0fd
Karol Łukaszuk <mazekpl@gmail.com>
2016-04-09 17:45:34 +0200

    Allocha :D

--- app/src/main/java/pl/angrymarschmallow/lubnews/MainActivity.java
+++ app/src/main/java/pl/angrymarschmallow/lubnews/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import org.json.JSONArray;
@@ -21,6 +24,7 @@ import java.util.concurrent.ExecutionException;
 public class MainActivity extends AppCompatActivity {
 
     ListView lv;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +32,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lv = (ListView) findViewById(R.id.listView);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), Main22Activity.class);
+                startActivity(intent);
+            }
+        });
 
-        List<Tag> array_list;
+        List<TagContent> array_list;
 
 
         try {
@@ -63,10 +75,10 @@ public class MainActivity extends AppCompatActivity {
 //        startActivity(intent);
     }
 
-    private class HttpAsyncTask extends AsyncTask<String, Void, List<Tag>> {
+    private class HttpAsyncTask extends AsyncTask<String, Void, List<TagContent>> {
         String data;
         @Override
-        protected List<Tag> doInBackground(String... params) {
+        protected List<TagContent> doInBackground(String... params) {
             HttpURLConnection connection = null;
             BufferedReader reader = null;
 
@@ -82,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
                 while ((line = reader.readLine()) != null){
                     buffer.append(line);
                 }
-                List<Tag> tags = new ArrayList<>();
+                List<TagContent> tags = new ArrayList<>();
 
                 String finalJson = buffer.toString();
                 data = buffer.toString();
@@ -90,15 +102,15 @@ public class MainActivity extends AppCompatActivity {
 
                 JSONArray parentArray = jsonObject.getJSONArray("tags");
                 for(int j =0;   j < parentArray.length(); j++) {
-                    Tag tag = new Tag();
+                    TagContent tag = new TagContent();
                     tag.setName(parentArray.getJSONObject(j).getString("name"));
                     tag.setNews_count(parentArray.getJSONObject(j).getInt("news_count"));
 
                     JSONArray jsonNewsArray = parentArray.getJSONObject(j).getJSONArray("news_list");
-                    List<Tag.News_list> news_list = new ArrayList<>();
+                    List<TagContent.News_list> news_list = new ArrayList<>();
 
                     for (int i = 0; i < jsonNewsArray.length(); i++) {
-                        Tag.News_list news = new Tag.News_list();
+                        TagContent.News_list news = new TagContent.News_list();
                         news.setDescription(jsonNewsArray.getJSONObject(i).getString("description"));
                         news.setTitle(jsonNewsArray.getJSONObject(i).getString("title"));
                         news.setUrl(jsonNewsArray.getJSONObject(i).getString("url"));
@@ -118,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(List<Tag> result) {
+        protected void onPostExecute(List<TagContent> result) {
             super.onPostExecute(result);
         }
     }
